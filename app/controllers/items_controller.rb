47899f5367848b2ee09d2367585f14418869059e
Rohan Patel <patel@Rohans-MacBook-Air-2.local>
2023-04-25 19:40:36 -0500

    added images

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -64,6 +64,6 @@ class ItemsController < ApplicationController
     ##
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :alternate_name, :price, :cost, :quantity, :sku)
+      params.require(:item).permit(:name, :alternate_name, :price, :cost, :quantity, :tax_rate_id, :sku, images: [])
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 ##
 # Item model
 class Item < ApplicationRecord
+  has_many_attached :images
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :modifier_groups
   belongs_to :tax_rate
